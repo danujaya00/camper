92d90c45dd4284873fc8ce6318a5ee716201bce0
Danujaya  Liyanage <2021is054@stu.ucsc.cmb.ac.lk>
2024-05-10 20:52:09 +0530

    Update layout and global styles

---
+++ components/Button.tsx
@@ -0,0 +1,15 @@
+type ButtonProps = {
+    type: 'button' | 'submit' ;
+    title: string;
+    icon: string;
+    variant: 'btn_dark_green';
+    }
+
+const Button = ({type, title, icon , variant}:ButtonProps) => {
+  return (
+    <button type={type}>Log In</button>
+   
+  )
+}
+
+export default Button
